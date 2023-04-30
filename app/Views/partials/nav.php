3a93b2cfce23de55381f15272a004ceb6d3dc21a
KinKinoV <mr.kolbasa.ask@gmail.com>
2023-04-30 18:45:17 +0300

    [feature/auth] Created partials to site's pages

---
+++ app/Views/partials/nav.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-light bg-info" style="--bs-bg-opacity: .4">
+    <div class="container-fluid" >
+        <a href="#" class="navbar-brand text-light">
+            <img src="<?= IMG_URL . '/TV-icon.ico' ?>" alt="siteIco" width="30" height="30" class="d-inline-block align-text-top">
+            TV Company
+        </a>
+    </div>
+    <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link disabled">Disabled</a>
+    </div>
+</nav>
